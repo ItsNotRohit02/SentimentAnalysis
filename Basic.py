de1648071940570cb57f3aa98251b901a66f57ca
Rohit <rohit2k02@gmail.com>
2022-12-27 17:11:01 +0530

    Initial commit

--- Basic.py
+++ Basic.py
@@ -6,6 +6,6 @@ import torch
 tokenizer = AT.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
 model = AM.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-tokens = tokenizer.encode('It was a horrible day', return_tensors='pt')
+tokens = tokenizer.encode('This is sample text', return_tensors='pt')
 result = model(tokens)
 print(int(torch.argmax(result.logits))+1)
--- SentimentApp.py
+++ SentimentApp.py
@@ -6,15 +6,16 @@ import torch
 st.set_page_config(page_title="Sentiment Analysis", page_icon=":smile:")
 
 with st.container():
-    st.title('Basic Sentiment Analysis :grin: :sob: :expressionless:')
-    st.write("This is a very basic Sentiment Analysis program that returns a value between -1 and 1 depending on the "
-             "text entered. The program makes use of basic Natural Language Processing Algorithms.")
+    st.title('Sentiment Analysis :grin: :sob: :expressionless:')
+    st.write("This is a Sentiment Analysis program that returns a value between 1 and 5 depending on the "
+             "text entered. The program makes use of a pre-trained Natural Language Processing Algorithm to identify "
+             "the Sentiment of the text entered.")
     st.write("---")
 
 with st.container():
     tokenizer = AT.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
     model = AM.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-    text = st.text_area('Enter Text Here', value='')
+    text = st.text_input('Enter Text Here', value='This is sample text')
     tokens = tokenizer.encode(text, return_tensors='pt')
     result = model(tokens)
     value = int(torch.argmax(result.logits)) + 1
@@ -32,9 +33,4 @@ with st.container():
     st.write(value)
     st.caption('Made by Rohit')
     st.markdown("[![Foo](https://cdn-icons-png.flaticon.com/24/25/25231.png)](https://github.com/ItsNotRohit02)")
-
-
-
-
-
 
